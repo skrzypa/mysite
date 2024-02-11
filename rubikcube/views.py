1c0815a5a635e71557d0990435f660bde5d74946
Patryk <patrykskrzynski@protonmail.com>
2024-02-11 14:42:20 +0100

    Style improvements in Rubik's cube tutorial

--- rubikcube/views.py
+++ rubikcube/views.py
@@ -18,19 +18,19 @@ def rubiccube(request):
     colors = Colors.objects.last()
 
     context = {"chapter_class": f"text-{colors.chapter_text} border border-dark",
-               "chapter_style": f"background-color: {colors.chapter_color}; padding: 0.5rem; border-radius: 5px;",
+               "chapter_style": f"background-color: {colors.chapter_color}; padding: 10px; border-radius: 15px;",
 
                "content_class": f"text-{colors.content_text} text-start border border-dark fs-4",
-               "content_style": f"background-color: {colors.content_color}; padding: 0.5rem; border-radius: 5px;",
+               "content_style": f"background-color: {colors.content_color}; padding: 10px; border-radius: 15px;",
                
                "menu_class": f"text-{colors.menu_text} border border-dark",
                "menu_style": f"background-color: {colors.menu_color};",
                "menu_link": f"{colors.menu_text}",
 
-               "alg_div_class": f"container-fluid text-center",
-               "alg_div_style": f"background-color: {colors.alg_color}; margin-top: 0.5rem; margin-bottom: 0.5rem;",
-               "alg_class": f"h1 text-{colors.alg_text}",
-               "alg_style": f"margin: 0.5rem;", 
+               "alg_div_class": f"container text-center",
+               "alg_div_style": f"background-color: {colors.alg_color}; margin-top: 0.5rem; margin-bottom: 0.5rem; padding: 0.5rem; border-radius: 15px;",
+               "alg_class": f"h3 text-{colors.alg_text}",
+               "alg_style": f"margin: 0; padding: 0;", 
 
                "link_color": f"{colors.content_text}"
                }
