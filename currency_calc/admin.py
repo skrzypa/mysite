5d2c1f4f5081b327e303f45b731f43fbaa59f43c
Patryk <patrykskrzynski@protonmail.com>
2024-05-20 17:25:30 +0200

    Admin for NBP API

--- currency_calc/admin.py
+++ currency_calc/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import NBP_API
 
 # Register your models here.
+class NBP_APIAdmin(admin.ModelAdmin):
+    list_display = ['id', 'currencies', 'date_addded']
+admin.site.register(NBP_API, NBP_APIAdmin)
--- currency_calc/models.py
+++ currency_calc/models.py
@@ -5,3 +5,7 @@ class NBP_API(models.Model):
     id = models.AutoField(primary_key= True)
     date_addded = models.DateTimeField(auto_now= True)
     currencies = models.JSONField()
+
+    class Meta:
+        verbose_name = "NBP API"
+        verbose_name_plural = "NBP API"
